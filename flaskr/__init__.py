3b4e9db576b436468141c53b22a6349fae4024ca
andwati <andwatiian@gmail.com>
2025-07-19 18:47:12 +0300

    setup auth

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -28,4 +28,7 @@ def create_app(test_config: dict[str, Any] | None = None) -> Flask:
     from . import db
     db.init_app(app)
 
+    from . import auth
+    app.register_blueprint(auth.bp)
+
     return app
